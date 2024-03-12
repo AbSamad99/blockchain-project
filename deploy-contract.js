05ad8e2b3fa05cf77d10a666511cb44ec09c96e5
AbSamad99 <abdussamad.mi5@gmail.com>
2024-03-12 16:23:06 -0400

    Adding JS files related to the contract deployment. Minor changes to docker related files.

---
+++ deploy-contract.js
@@ -0,0 +1,15 @@
+var deployedContract = contract.new({
+  data: bytecode,
+  from: eth.accounts[0],
+  gas: 2000000,
+});
+
+while (eth.getTransactionReceipt(deployedContract.transactionHash) === null) {
+  // Wait for the transaction to be mined
+}
+
+var contractAddress = eth.getTransactionReceipt(
+  deployedContract.transactionHash
+).contractAddress;
+
+var instance = contract.at(contractAddress);
