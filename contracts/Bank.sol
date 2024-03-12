f6c2663969d0796dd4cb072206a81adf921a36f6
AbSamad99 <abdussamad.mi5@gmail.com>
2024-03-12 11:24:28 -0400

    Minor fixes and changes to the Bank contract.

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+
 /// @title Banking Applicaion
 
 contract Bank {
     struct TransferRequest {
         address to;
-        uint amount;
+        uint256 amount;
     }
 
     struct Account {
         string name;
-        uint balance;
+        uint256 balance;
         address accountNumber;
         TransferRequest[] transferRequests;
     }
@@ -49,7 +50,7 @@ contract Bank {
         _;
     }
 
-    modifier performBalanceCheck(uint amount) {
+    modifier performBalanceCheck(uint256 amount) {
         require(
             accounts[msg.sender].balance >= amount,
             "Insufficient balance!"
@@ -57,15 +58,24 @@ contract Bank {
         _;
     }
 
-    modifier validateTransferReqIndex(uint index) {
+    modifier validateTransferReqIndex(uint256 index) {
         require(
             index < accounts[msg.sender].transferRequests.length,
             "No transfer request at that index!"
         );
+        _;
+    }
+
+    modifier checkSameAccount(address addressToCheck) {
+        require(
+            msg.sender != addressToCheck,
+            "Cannot make request to yourself!"
+        );
+        _;
     }
 
     // Open your new account
-    function openAccount(string name) public requireNoAccount {
+    function openAccount(string calldata name) public requireNoAccount {
         require(
             bytes(name).length >= 5,
             "Name must be at least 5 characters long!"
@@ -77,33 +87,31 @@ contract Bank {
     }
 
     // Add funds to your account
-    function addFunds() public requireAccount {
-        require(msg.value % 1000 != 0, "Amount has to be a multiple of 1000!");
-        uint amountToBeAdded = msg.value / 1000;
+    function addFunds() public payable requireAccount {
+        require(msg.value % 1000 == 0, "Amount has to be a multiple of 1000!");
+        uint256 amountToBeAdded = msg.value / 1000;
         accounts[msg.sender].balance += amountToBeAdded;
     }
 
     // Withdraw funds from your account
     function withdrawFunds(
-        uint amount
+        uint256 amount
     ) public requireAccount performBalanceCheck(amount) {
-        (bool sent, bytes memory data) = msg.sender.call{value: amount * 1000}(
-            ""
-        );
+        (bool sent, ) = msg.sender.call{value: amount * 1000}("");
         require(sent, "Failed to withdraw funds!");
         accounts[msg.sender].balance -= amount;
     }
 
     // Check the balance in your account
-    function checkBalance() public view requireAccount returns (uint) {
+    function checkBalance() public view requireAccount returns (uint256) {
         return accounts[msg.sender].balance;
     }
 
     // Transfers funds to a different account
     function transferFunds(
         address to,
-        uint amount
-    ) public requireAccount performBalanceCheck(amount) {
+        uint256 amount
+    ) public requireAccount performBalanceCheck(amount) checkSameAccount(to) {
         require(
             bytes(accounts[to].name).length != 0,
             "Recipient account does not exist!"
@@ -113,7 +121,10 @@ contract Bank {
     }
 
     // Request transfer of funds from another account
-    function requestFunds(address from, uint amount) public requireAccount {
+    function requestFunds(
+        address from,
+        uint256 amount
+    ) public requireAccount checkSameAccount(from) {
         require(
             bytes(accounts[from].name).length != 0,
             "Requested account does not exist!"
@@ -124,32 +135,37 @@ contract Bank {
     }
 
     // Check the number of transfer requests made to your account
-    function checkTransferReqNum() public view requireAccount returns (uint) {
+    function checkTransferReqNum()
+        public
+        view
+        requireAccount
+        returns (uint256)
+    {
         return accounts[msg.sender].transferRequests.length;
     }
 
     // Check transfer request details by index
     function checkTransferReqDetails(
-        uint index
+        uint256 index
     )
         public
         view
         requireAccount
         validateTransferReqIndex(index)
-        returns (TransferRequest)
+        returns (TransferRequest memory)
     {
         return accounts[msg.sender].transferRequests[index];
     }
 
     // Approve transfer request by index
     function approveTransferRequest(
-        uint index
+        uint256 index
     ) public requireAccount validateTransferReqIndex(index) {
-        TransferRequest memory request = accounts[msg.sender].transferRequests[
+        TransferRequest storage request = accounts[msg.sender].transferRequests[
             index
         ];
         transferFunds(request.to, request.amount);
-        uint lastIndex = accounts[msg.sender].transferRequests.length - 1;
+        uint256 lastIndex = accounts[msg.sender].transferRequests.length - 1;
         accounts[msg.sender].transferRequests[index] = accounts[msg.sender]
             .transferRequests[lastIndex];
         accounts[msg.sender].transferRequests.pop();
@@ -157,30 +173,34 @@ contract Bank {
 
     // Close your account and then transfer your balance as wei to your address
     function closeAccount() public requireAccount {
-        uint balance = accounts[msg.sender].balance;
+        uint256 balance = accounts[msg.sender].balance;
         delete accounts[msg.sender];
         if (balance > 0) {
             (bool sent, ) = msg.sender.call{value: balance * 1000}("");
             require(sent, "Failed to transfer balance to account holder!");
         }
 
-        for (uint i = 0; i < activeAccounts.length; i++) {
+        for (uint256 i = 0; i < activeAccounts.length; i++) {
             if (activeAccounts[i] == msg.sender) {
                 activeAccounts[i] = activeAccounts[activeAccounts.length - 1];
-                accountIndex.pop();
+                activeAccounts.pop();
                 break;
             }
         }
     }
 
     // Close the bank
-    function closeBank() public onlyBankManager {
-        for (uint i = 0; i < accountIndex.length; i++) {
-            address accountAddress = accountIndex[i];
-            uint balance = balances[accountAddress];
-            (bool sent, ) = accountAddress.call{value: balance * 1000}("");
-            require(sent, "Failed to transfer balance to account holder");
-        }
-        selfdestruct(payable(bankManager));
-    }
+    // function closeBank() public onlyBankManager {
+    //     for (uint i = 0; i < activeAccounts.length; i++) {
+    //         address accountAddress = activeAccounts[i];
+    //         uint balance = accounts[accountAddress].balance;
+    //         (bool sent, ) = accountAddress.call{value: balance * 1000}("");
+    //         require(sent, "Failed to transfer balance to account holder");
+    //     }
+    //     selfdestruct(payable(bankManager));
+    // }
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
